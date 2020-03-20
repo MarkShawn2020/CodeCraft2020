98676805034e1897c4fbe071c79c536264e025a8
MarkShawn2020 <877210964@qq.com>
2020-03-20 08:29:43 +0800

    del data and release for some consideraton

--- release/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# -----------------------------------
-# @CreateTime   : 2020/3/20 2:45
-# @Author       : Mark Shawn
-# @Email        : shawninjuly@gmail.com
-# ------------------------------------
